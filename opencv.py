35cd9e29eb282c03a3e4e7957e03db5ab6a2c9e0
ChargerAirHockey <chargerairhockey@gmail.com>
2019-04-25 22:05:36 -0500

    After presentation

--- opencv.py
+++ opencv.py
@@ -8,34 +8,46 @@ import socket
 import sys
 import serial
 import math 
-##try:
-##    ser = serial.Serial("/dev/ttyACM0",9600,timeout=10)
-##    if ser.is_open:
-##        print ("Connection ACM0 opened..")
-##except:
-##    ser = serial.Serial("/dev/ttyACM1",9600,timeout=10)
-##    if ser.is_open:
-##        print ("Connection ACM1 opened..")
 
-s = socket.socket()         # Create a socket object
-host = "192.168.0.1"    # Get local machine name
-port = 5000                # Reserve a port for your service.
+## Code modified for use with both PIDRobot and ML Robot
+## To use PIDRobot only set PID_flag == True and ML_flag = False
+## To use MLRobot only ML_flag == True and PID_flag = False
+PID_flag = True
+ML_flag = False
 
-print ('Server started!')
-print('Waiting for clients...')
+if PID_flag:
+    # Set up serial connection to Arduino
+    try:
+        ser = serial.Serial("/dev/ttyACM0",9600,timeout=10)
+        if ser.is_open:
+            print ("Connection ACM0 opened..")
+    except:
+        ser = serial.Serial("/dev/ttyACM4",9600,timeout=10)
+        if ser.is_open:
+            print ("Connection ACM1 opened..")
 
-#s.bind((host, port))        # Bind to the port
-#s.listen(5)                 # Now wait for client connection.
-#c, addr = s.accept()        # Establish connection with client.
-#print('Got connection from', addr)
 
+if ML_flag:
+    # Set up TCP Connection to ML Raspberry PI
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)         # Create a socket object
+    host = "192.168.0.2"        # Get local machine name
+    ##host = socket.gethostname() # Get local machine name
+    port = 5000                 # Reserve a port for your service.
+
+    print ('Server started!')
+    print('Waiting for clients...')
+
+    s.bind((host, port))        # Bind to the port
+    s.listen(5)                 # Now wait for client connection.
+    c, addr = s.accept()        # Establish connection with client.
+    print('Got connection from', addr)
+
+# Receving Image processing data
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH,320)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT,240)
 cap.set(cv2.CAP_PROP_FPS,120)
 print("FPS", cap.get(cv2.CAP_PROP_FPS))
-
-
 
 #cap.set(cv2.CAP_PROP_FRAME_COUNT,45)
 
@@ -58,10 +70,11 @@ while(1):
     (h,w) = frame.shape[:2]
     center = (w/2,h/2)
 
-    M = cv2.getRotationMatrix2D(center,-2.5,1.0)
+    M = cv2.getRotationMatrix2D(center,-0.2,1)
     rotated = cv2.warpAffine(frame,M,(w,h))
     frame = rotated
-    frame = frame[40:210,0:320]
+    frame = frame[40:200,5:320]
+    
 
     # Convert BGR to HSV
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -79,6 +92,9 @@ while(1):
 
     # Bitwise-AND mask and original image
     res = cv2.bitwise_and(frame,frame, mask= mask)
+
+    cv2.imwrite("hockey.jpg",frame)
+    cv2.imwrite("mask.jpg",mask)
 
     cv2.imshow('frame',frame)
     cv2.imshow('mask',mask)
@@ -119,10 +135,10 @@ while(1):
         elif(x > 350):
             x = 350
 
-        if(y < 0):
-            y = 0
-        elif(y > 300):
-            y = 300
+        if(y <= 0):
+            y = 1
+        elif(y >= 300):
+            y = 299
 
         if(oldx == 0 and oldy == 0):
             oldx = x
@@ -133,60 +149,80 @@ while(1):
         
         count+=1
 
-        if(count == 1):
+        if(count == 2):
             #print("original x:{} original y:{} ".format(int(oldx),int(oldy)))
             #print("mew x     :{} new y     :{}".format(int(x),int(y)))
 
             outx,outy = algorithm(int(oldx),int(oldy),int(x),int(y))
-        
-            
-            if(outx != -900 and outy != -900):
-                number_range.insert(0,outy)
-                if(abs(number_range[0]-number_range[1]) < 50 and
-                   abs(number_range[0]-number_range[2]) < 50 and
-                   abs(number_range[1]-number_range[2]) < 50):
-                    if(outx > 0):
-                        print("impact x  :{} impact y  :{}".format(int(outx),int(number_range[0])))
-                oldx = x
-                oldy = y
-                number_range.pop()
-##
-##                inbuf = str(int(outy))
-##                ser.write(inbuf.encode(encoding='ascii'))
-                #print ("Sent " + inbuf)
-##                time.sleep(5) 
-
             count = 0
-            
-            
-        #if(x != -500 and y != -500):
-         #   #print("x:{} y:{} Time:{}".format(int(x),int(y),end-start))
 
-            #final serial code
-          #  outx,outy = algorithm(oldx,oldy,x,y)
+            if PID_flag:
+                print("x:{} y:{}".format(int(x),int(y)))
+                ##PID Control for Arduino
+                if(outx != -900 and outy != -900):
+                    number_range.insert(0,outy)
+                    if(abs(number_range[0]-number_range[1]) < 50 and
+                       abs(number_range[0]-number_range[2]) < 50 and
+                       abs(number_range[1]-number_range[2]) < 50):
+                        if(outx < 0):
+                            
+                            #print("original x:{} original y:{} ".format(int(oldx),int(oldy)))
+                            #print("mew x     :{} new y     :{}".format(int(x),int(y)))
+                            
+                            if(x > -150):
+                                #print("impact x  :{} impact y  :{}".format(int(outx),int(number_range[0])))                        
+                                Xbuff = ("${}$".format(int(number_range[0])))
+                                #inbuf = str(int(number_range[0]))
+                                #print(Xbuff)
+                                ser.write(Xbuff.encode(encoding='ascii'))
+                            else:
+                                XspinBuf = "$999$"
+                                #print(XspinBuf)
+                                ser.write(XspinBuf.encode(encoding='ascii'))
+                        elif(outx > 0):
+                            
+                            #print("original x:{} original y:{} ".format(int(oldx),int(oldy)))
+                            #print("mew x     :{} new y     :{}".format(int(x),int(y)))
+                            
+                            if(x < 150):
+                                #print("impact x  :{} impact y  :{}".format(int(outx),int(number_range[0])))                        
+                                Ybuff = ("<{}<".format(int(number_range[0])))
+                                #inbuf = str(int(number_range[0]))
+                                #print(Ybuff)
+                                ser.write(Ybuff.encode(encoding='ascii'))
+                            else:
+                                YspinBuf = "<999<"
+                                #print(YspinBuf)
+                                ser.write(YspinBuf.encode(encoding='ascii'))
+                            
+                    oldx = x
+                    oldy = y
+                    number_range.pop()
+             
+        if ML_flag:
+            # Modified TCP code, sending x and y (no longer checking outx&outy)    
+            if(x != -500 and y != -500):
+                #print("x:{} y:{} Time:{}".format(int(x),int(y),end-start))
 
-           # if(outx != -900 and outy != -900):
-            #    if(outy > 0 and outy < 350):
-             #       message = ("{},{}".format(int(x),int(y)))
-              #      print("x:{} y:{}".format(int(x),int(y)))
-               #     try:
-                #        recv = c.recv(1024)
-                 #       recv = recv.decode()
-                  #      if(recv == "ready"):
-                   #         c.send(message.encode())
-                            
-                   # except (BrokenPipeError, IOError):
-                    #    print("error occured")
-                     #   sys.exit()
-                        
-                            
-                    
-        
+                #final TCP code
+                message = ("{},{}".format(int(x),int(y)))
+                print("x:{} y:{}".format(int(x),int(y)))
+                try:
+                    ready = False
+                    while not ready:
+                        recv = c.recv(1024)
+                        recv = recv.decode()
+                        if(recv == "ready"):
+                            c.send(message.encode())
+                            ready = True           
+                except (BrokenPipeError, IOError):
+                    print("error occured")
+                    sys.exit()
+
+            # I don't think this is used any more (oldx and oldy are set above) - Stephen
             #old_outy = outy
             #oldx = x
             #oldy = y
-
-    
     
     #cv2.waitKey()
 
@@ -195,6 +231,6 @@ while(1):
         break
 
 message = "close"
-#c.send(message.encode())
-#s.close()
+c.send(message.encode())
+s.close()
 cv2.destroyAllWindows()
